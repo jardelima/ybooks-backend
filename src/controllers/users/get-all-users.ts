41591e4fe1c1bfa454ef413314cd52af558fd24d
Jardel Lima <prof_jardel@hotmail.com>
2025-02-25 20:55:41 -0300

    Adicionar controller de pegar todos usuários

---
+++ src/controllers/users/get-all-users.ts
@@ -0,0 +1,18 @@
+import { Response } from "express";
+
+import { AppDataSource } from "../../database/data-source";
+import { User } from "../../database/entity/user.entity";
+
+const userRepository = AppDataSource.getRepository(User);
+
+export const getAllUsers = async (res: Response): Promise<Response> => {
+    try {
+        const users = await userRepository.find();
+
+        return res.status(200).json(users);
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Erro ao fazer a requisicão para pegar os usuários. \n", error });
+    }
+};
