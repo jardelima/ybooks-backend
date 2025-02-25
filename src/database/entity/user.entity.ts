e9d16a9fd8ff4fa0f4d7f50bc77b04f350c27417
Jardel Lima <prof_jardel@hotmail.com>
2025-02-25 20:57:31 -0300

    Remover constructor desnecessário da entity

--- src/database/entity/user.entity.ts
+++ src/database/entity/user.entity.ts
@@ -2,26 +2,18 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id;
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
 
     @Column({ type: "varchar", length: 11, nullable: false })
-    cpf;
+    cpf!: string;
 
     @Column({ type: "text" })
-    name;
+    name!: string;
 
-    @Column({ type: "varchar", length: 8 })
-    birthday;
+    @Column({ type: "varchar" })
+    birthday!: string;
 
     @Column({ type: "text" })
-    address;
-
-    constructor(id: number, cpf: string, name: string, birthday: string, address: string) {
-        this.id = id;
-        this.cpf = cpf;
-        this.name = name;
-        this.birthday = birthday;
-        this.address = address;
-    }
+    address!: string;
 }
