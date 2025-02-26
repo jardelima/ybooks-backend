10ddb876889528f510fec4a58f2c485789dce508
Jardel Lima <prof_jardel@hotmail.com>
2025-02-26 18:31:22 -0300

    Adicionar alias import e adicionar rotas de delete e update

--- src/routes/users/users-routes.ts
+++ src/routes/users/users-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
-import { getAllUsers } from "../../controllers/users/get-all-users";
-import { registerUser } from "../../controllers/users/register-user";
+import { deleteUser } from "@/controllers/users/delete-user";
+import { getAllUsers } from "@/controllers/users/get-all-users";
+import { registerUser } from "@/controllers/users/register-user";
+import { updateUser } from "@/controllers/users/update-user";
 
 const router = Router();
 
@@ -22,6 +24,28 @@ router.post("/users", async (req, res) => {
     } catch (error) {
         res.status(500).json({
             message: "Erro ao processar a requisicão de cadastrar usuário. \n",
+            error,
+        });
+    }
+});
+
+router.delete("/users/:id", async (req, res) => {
+    try {
+        await deleteUser(req, res);
+    } catch (error) {
+        res.status(500).json({
+            message: "Erro ao processar a requisicão de remover usuário. \n",
+            error,
+        });
+    }
+});
+
+router.patch("/users/:id", async (req, res) => {
+    try {
+        await updateUser(req, res);
+    } catch (error) {
+        res.status(500).json({
+            message: "Erro ao processar a requisicão de remover usuário. \n",
             error,
         });
     }
