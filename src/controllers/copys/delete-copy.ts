0e9657b75cfb23925c373e4aa8b0e07bb07c9621
Jardel Lima <prof_jardel@hotmail.com>
2025-02-27 20:42:48 -0300

    Adicionar controller de remover copia

---
+++ src/controllers/copys/delete-copy.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+
+import { AppDataSource } from "@/database/data-source";
+import { Book } from "@/database/entity/books.entity";
+import { Copy } from "@/database/entity/copy.entity";
+import { CopyModel } from "@/models/copy.model";
+
+const bookRepository = AppDataSource.getRepository(Book);
+const copyRepository = AppDataSource.getRepository(Copy);
+
+export const deleteCopy = async (
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    req: Request<{ id: string; copyId: string }, {}, CopyModel>,
+    res: Response,
+): Promise<Response> => {
+    try {
+        const { id, copyId } = req.params;
+
+        const book = await bookRepository.findOneBy({ id });
+
+        const copys = await bookRepository.findOne({
+            where: { id },
+            relations: ["copys"],
+        });
+
+        if (!copys || !book) {
+            return res.status(404).json({ message: `O livro com ID ${id} não existe.` });
+        }
+
+        const copy = copys.copys.find((copy) => copy.id === copyId);
+
+        if (!copy) {
+            return res.status(404).json({ message: `A cópia com ID ${id} não existe.` });
+        }
+
+        await copyRepository.remove(copy);
+
+        return res.status(200).json({
+            message: `Cópia do livro ${book.title} deletada com sucesso!`,
+            copy,
+        });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: `Erro ao tentar deletar cópia do livro. \n`, error });
+    }
+};
