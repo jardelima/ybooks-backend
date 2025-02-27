64cb8da304ac83c4c607a9754e4dc0c99c20bbe9
Jardel Lima <prof_jardel@hotmail.com>
2025-02-27 20:43:05 -0300

    Adicionar controller de pegar todas as copia de um livro

---
+++ src/controllers/copys/get-all-copys.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+
+import { AppDataSource } from "@/database/data-source";
+import { Book } from "@/database/entity/books.entity";
+import { CopyModel } from "@/models/copy.model";
+
+const bookRepository = AppDataSource.getRepository(Book);
+
+export const getAllCopys = async (
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    req: Request<{ id: string }, {}, CopyModel>,
+    res: Response,
+): Promise<Response> => {
+    try {
+        const { id } = req.params;
+
+        const book = await bookRepository.findOneBy({ id });
+
+        const copys = await bookRepository.findOne({
+            where: { id },
+            relations: ["copys"],
+        });
+
+        if (!copys || !book) {
+            return res.status(404).json({ message: `O livro com ID ${id} não existe.` });
+        }
+
+        return res.status(201).json({
+            message: `Aqui estão todas as cópias do livro ${book.title} cadastrada no sistema.`,
+            copys,
+        });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: `Erro ao tentar listar cópias do livro. \n`, error });
+    }
+};
