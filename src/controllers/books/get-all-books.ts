87d5bec12119b89009b77476a8c29c3bb02ae491
Jardel Lima <prof_jardel@hotmail.com>
2025-02-27 17:17:56 -0300

    Adicionar controller de ver todos livros

---
+++ src/controllers/books/get-all-books.ts
@@ -0,0 +1,18 @@
+import { Response } from "express";
+
+import { AppDataSource } from "@/database/data-source";
+import { Book } from "@/database/entity/books.entity";
+
+const bookRepository = AppDataSource.getRepository(Book);
+
+export const getAllBooks = async (res: Response): Promise<Response> => {
+    try {
+        const books = await bookRepository.find();
+
+        return res.status(200).json(books);
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Erro ao fazer a requisicão para listar todos os livros. \n", error });
+    }
+};
