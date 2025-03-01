0cc6057694dfa9dd5c22b709cdff20fa97afd235
Jardel Lima <prof_jardel@hotmail.com>
2025-02-28 21:14:46 -0300

    Adicionar rotas de admin e login

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import express from "express";
 
 import { initializeDatabase } from "./database/data-source";
 // Routes
+import admin from "./routes/admin/admin-routes";
 import books from "./routes/books/books-routes";
 import copys from "./routes/copys/copys-routes";
+import login from "./routes/login/login-routes";
 import users from "./routes/users/users-routes";
 
 const app = express();
@@ -13,9 +15,11 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", admin);
 app.use("/api", users);
 app.use("/api", books);
 app.use("/api", copys);
+app.use("/api", login);
 
 // Iniciar o banco de dados
 initializeDatabase();
