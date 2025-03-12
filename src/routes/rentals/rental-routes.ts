adf61d4cdb40a405d3d19ba1e373650a89239cee
Jardel Lima <prof_jardel@hotmail.com>
2025-03-12 10:23:22 -0300

    Adicionar rota de livros que mais atrasao

--- src/routes/rentals/rental-routes.ts
+++ src/routes/rentals/rental-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { pickMostDelayed } from "@/controllers/rentals/pick-most-delayed";
 import { pickMostRented } from "@/controllers/rentals/pick-most-rented";
 import { rentBook } from "@/controllers/rentals/rental-book";
 import { returnBook } from "@/controllers/rentals/return-rental-book";
@@ -35,6 +36,17 @@ router.get("/top-rented", async (_, res) => {
     } catch (error) {
         res.status(500).json({
             message: "Erro ao processar a requisicão de listar top três livros. \n",
+            error,
+        });
+    }
+});
+
+router.get("/top-delayed", async (_, res) => {
+    try {
+        await pickMostDelayed(res);
+    } catch (error) {
+        res.status(500).json({
+            message: "Erro ao processar a requisicão de listar livros em atrasos. \n",
             error,
         });
     }
